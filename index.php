ba2cb8569023c5e5cf3f3a93b46d97d2648c2e7a
Soham Tamboli <sohamtamboli@gmail.com>
2020-12-14 00:49:05 +0530

    Added validation to form
    Regex validated form

--- index.php
+++ index.php
@@ -3,12 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link
+			href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+			rel="stylesheet"
+		/>
+		
+    <title>Index</title>
 </head>
 <body>
-    Hello World!
+   
     <?php
-    require('./connect.php'); 
+    require('./form.php'); 
     ?>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
